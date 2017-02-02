985b476365ddf29cdf8fec2ec2849e49b0964f1d
Elvis Nuñez <elvisnunez@me.com>
2017-02-02 15:35:09 +0100

    Initial Commit

---
+++ MixedFramework/MixedFramework.h
@@ -0,0 +1,19 @@
+//
+//  MixedFramework.h
+//  MixedFramework
+//
+//  Created by Elvis Nuñez on 02/02/2017.
+//  Copyright © 2017 MixedFramework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MixedFramework.
+FOUNDATION_EXPORT double MixedFrameworkVersionNumber;
+
+//! Project version string for MixedFramework.
+FOUNDATION_EXPORT const unsigned char MixedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MixedFramework/PublicHeader.h>
+
+
